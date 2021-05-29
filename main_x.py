c10c36aed818b04253e4fc739f71a65d410ab423
Karthik Ram <karthikram570@gmail.com>
2021-05-29 23:30:14 +0530

    adding data

--- main_x.py
+++ main_x.py
@@ -6,10 +6,14 @@ Created on Wed Apr 21 13:06:01 2021
 """
 
 import pandas as pd
+import numpy as np
+
 import os
 
 
 os.getcwd() 
 
-test_set = pd.read_csv("datasets/text.csv")
-train_set = pd.read_csv("datasets/train.csv")
+test_set = pd.read_csv("data/test.csv")
+train_set = pd.read_csv("data/train.csv")
+print("Data Set Columns: {}".format(train_set.columns))
+print(train_set.head())
